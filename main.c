5565db90d3b5105b5d943d28fd3c19eb53dbed16
soomin0409 <55428420+soomin0409@users.noreply.github.com>
2019-09-26 16:13:51 +0900

    06

--- main.c
+++ main.c
@@ -5,11 +5,15 @@
 
 int main(int argc, char *argv[]) {
 
-	int x= 3;
-	int cond1, cond2;
-	cond1 =2<x&&x<5;
-	cond2 =x>1|| x<4&&x>3;
-	printf("cond1=%d,cond2=%d",cond1,cond2);
+	int x,y;
 	
+	printf("enter 2 integers : ");
+	scanf("%d %d",&x,&y);
+	
+	printf("+ result: %i\n",x+y);
+	printf("- result: %i\n",x-y);
+	printf("* result: %i\n",x*y);
+	printf("/ result: %i\n",x/y);
+	printf("%% result:%i\n",x%y);
 	return 0;
 }
